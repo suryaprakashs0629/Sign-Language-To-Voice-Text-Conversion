5538ae27d89ec9582475bb89acad0d1b881a0854
suryaprakashs0629 <suryaprakash.s0629@gmail.com>
2026-06-10 20:57:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const textOutput = document.getElementById("textOutput");
+const confidenceOutput = document.getElementById("confidenceOutput");
+
+let currentText = "";
